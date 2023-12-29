e7c294c22ca08dd85b025a09ee5bab30965cef4a
sailor8686 <96840943+sailor8686@users.noreply.github.com>
2023-12-29 16:25:34 +0300

    Create TR.php
    Complete list of provinces and districts in Turkiye.

---
+++ cities/TR.php
@@ -0,0 +1,176 @@
+<?php
+
+/*
+
+* Turkiye Cities
+by Sailor86
+
+*/
+
+global $cities;
+
+$cities['TR'] = array(
+
+	    'TR01' => array('Merkez','Ceyhan','Çukurova','Feke','İmamoğlu','Karaisalı','Karataş','Kozan','Pozantı','Saimbeyli','Sarıçam','Seyhan','Tufanbeyli','Yumurtalık','Yüreğir'),
+
+		'TR02'=> array('Merkez','Besni','Çelikhan','Gerger','Gölbaşı','Kâhta','Samsat','Sincik','Tut'),
+
+		'TR03' => array('Merkez','Başmakçı','Bayat','Bolvadin','Çay','Çobanlar','Dazkırı','Dinar','Emirdağ','Evciler','Hocalar','İhsaniye','İscehisar','Kızılören','Sandıklı','Sinanpaşa','Sultandağı','Şuhut'),
+
+		'TR04' => array('Merkez','Diyadin','Doğubayazıt','Eleşkirt','Hamur','Patnos','Taşlıçay','Tutak'),
+
+		'TR05' => array('Merkez','Göynücek','Gümüşhacıköy','Hamamözü','Merzifon','Suluova','Taşova'),
+
+		'TR06' => array('Akyurt','Altındağ','Ayaş','Balâ','Beypazarı','Çamlıdere','Çankaya','Çubuk','Elmadağ','Etimesgut','Evren','Gölbaşı','Güdül','Haymana','Kalecik','Kahramankazan','Keçiören','Kızılcahamam','Mamak','Nallıhan','Polatlı','Pursaklar','Sincan','Şereflikoçhisar','Yenimahalle'),		
+
+		'TR07' => array('Akseki','Aksu','Alanya','Döşemealtı','Elmalı','Finike','Gazipaşa','Gündoğmuş','İbradı','Demre','Kaş','Kemer','Kepez','Konyaaltı','Korkuteli','Kumluca','Manavgat','Muratpaşa','Serik'),
+
+		'TR08' => array('Ardanuç','Arhavi','Merkez','Borçka','Hopa','Merkez','Murgul','Şavşat','Yusufeli'),
+
+		'TR09' => array('Bozdoğan','Buharkent','Çine','Didim','Efeler','Germencik','İncirliova','Karacasu','Karpuzlu','Koçarlı','Köşk','Kuşadası','Kuyucak','Nazilli','Söke','Sultanhisar','Yenipazar'),	
+
+		'TR10' => array('Altıeylül','Ayvalık','Balya','Bandırma','Bigadiç','Burhaniye','Dursunbey','Edremit','Erdek','Gömeç','Gönen','Havran','İvrindi','Karesi','Kepsut','Manyas','Marmara','Savaştepe','Sındırgı','Susurluk','Merkez'),	
+
+		'TR11' => array('Merkez','Bozüyük','Gölpazarı','İnhisar','Osmaneli','Pazaryeri','Söğüt','Yenipazar'),
+
+		'TR12' => array('Adaklı','Merkez','Genç','Karlıova','Kiğı','Merkez','Solhan','Yayladere','Yedisu'),
+
+		'TR13' => array('Adilcevaz','Ahlat','Merkez','Güroymak','Hizan','Mutki','Tatvan'),
+
+		'TR14' => array('Merkez','Dörtdivan','Gerede','Göynük','Kıbrıscık','Mengen','Mudurnu','Seben','Yeniçağa'),
+	
+		'TR15' => array('Ağlasun','Altınyayla','Bucak','Merkez','Çavdır','Çeltikçi','Gölhisar','Karamanlı','Kemer','Merkez','Tefenni','Yeşilova'),	
+
+		'TR16' => array('Büyükorhan','Gemlik','Gürsu','Harmancık','İnegöl','İznik','Karacabey','Keles','Kestel','Mudanya','Mustafakemalpaşa','Nilüfer','Orhaneli','Orhangazi','Osmangazi','Yenişehir','Yıldırım','Merkez'),
+
+		'TR17' => array('Merkez','Ayvacık','Bayramiç','Biga','Bozcaada','Çan','Merkez','Eceabat','Ezine','Gelibolu','Gökçeada','Lapseki','Yenice'),
+
+		'TR18' => array('Atkaracalar','Bayramören','Merkez','Çerkeş','Eldivan','Ilgaz','Kızılırmak','Korgun','Kurşunlu','Merkez','Orta','Şabanözü','Yapraklı'),
+
+		'TR19' => array('Alaca','Bayat','Boğazkale','Merkez','Dodurga','İskilip','Kargı','Laçin','Mecitözü','Merkez','Oğuzlar','Ortaköy','Osmancık','Sungurlu','Uğurludağ'),
+
+		'TR20' => array('Acıpayam','Babadağ','Baklan','Bekilli','Beyağaç','Bozkurt','Buldan','Çal','Çameli','Çardak','Çivril','Güney','Honaz','Kale','Merkez','Merkezefendi','Pamukkale','Sarayköy','Serinhisar','Tavas'),
+
+		'TR21' => array('Bağlar','Bismil','Çermik','Çınar','Çüngüş','Dicle','Eğil','Ergani','Hani','Hazro','Kayapınar','Kocaköy','Kulp','Merkez','Lice','Silvan','Sur','Yenişehir'),
+
+		'TR22' => array('Enez','Havsa','İpsala','Keşan','Lalapaşa','Meriç','Merkez','Süloğlu','Uzunköprü'),
+
+		'TR23' => array('Ağın','Alacakaya','Arıcak','Baskil','Elâzığ','Karakoçan','Keban','Kovancılar','Maden','Merkez','Palu','Sivrice'),
+
+		'TR24' => array('Çayırlı','Merkez','İliç','Kemah','Kemaliye','Merkez','Otlukbeli','Refahiye','Tercan','Üzümlü'),
+
+		'TR25' => array('Aşkale','Aziziye','Çat','Hınıs','Horasan','İspir','Karaçoban','Karayazı','Köprüköy','Merkez','Narman','Oltu','Olur','Palandöken','Pasinler','Pazaryolu','Şenkaya','Tekman','Tortum','Uzundere','Yakutiye'),
+
+		'TR26' => array('Alpu','Beylikova','Çifteler','Günyüzü','Han','İnönü','Mahmudiye','Merkez','Mihalgazi','Mihalıççık','Odunpazarı','Sarıcakaya','Seyitgazi','Sivrihisar','Tepebaşı'),
+
+		'TR27' => array('Araban','İslahiye','Karkamış','Nizip','Merkez','Nurdağı','Oğuzeli','Şahinbey','Şehitkâmil','Yavuzeli'),
+
+		'TR28' => array('Alucra','Bulancak','Çamoluk','Çanakçı','Dereli','Doğankent','Espiye','Eynesil','Giresun','Görele','Güce','Keşap','Merkez','Piraziz','Şebinkarahisar','Tirebolu','Yağlıdere'),
+
+		'TR29' => array('Kelkit','Köse','Kürtün','Merkez','Şiran','Torul'),
+
+		'TR30' => array('Çukurca','Merkez','Şemdinli','Yüksekova'),
+
+		'TR31' => array('Altınözü','Antakya','Arsuz','Belen','Defne','Dörtyol','Erzin','Hassa','İskenderun','Merkez','Kırıkhan','Kumlu','Payas','Reyhanlı','Samandağ','Yayladağı'),
+
+		'TR32' => array('Aksu','Atabey','Eğirdir','Gelendost','Gönen','Keçiborlu','Merkez','Senirkent','Sütçüler','Şarkikaraağaç','Uluborlu','Yalvaç','Yenişarbademli'),
+
+		'TR33' => array('Akdeniz','Anamur','Aydıncık','Bozyazı','Çamlıyayla','Erdemli','Gülnar','Mezitli','Mut','Silifke','Tarsus','Toroslar','Yenişehir'),
+
+		'TR34' => array('Adalar','Arnavutköy','Ataşehir','Avcılar','Bağcılar','Bahçelievler','Bakırköy','Başakşehir','Bayrampaşa','Beşiktaş','Beykoz','Beylikdüzü','Beyoğlu','Büyükçekmece','Çatalca','Çekmeköy','Esenler','Esenyurt','Eyüp','Fatih','Gaziosmanpaşa','Güngören','Kadıköy','Kağıthane','Kartal','Küçükçekmece','Maltepe','Pendik','Sancaktepe','Sarıyer','Silivri','Sultanbeyli','Sultangazi','Şile','Şişli','Tuzla','Ümraniye','Üsküdar','Zeytinburnu'),
+
+		'TR35' => array('Aliağa','Balçova','Bayındır','Bayraklı','Bergama','Beydağ','Bornova','Buca','Çeşme','Çiğli','Dikili','Foça','Gaziemir','Güzelbahçe','Karabağlar','Karaburun','Karşıyaka','Kemalpaşa','Kınık','Kiraz','Konak','Menderes','Menemen','Narlıdere','Ödemiş','Seferihisar','Selçuk','Tire','Torbalı','Urla'),
+
+		'TR36' => array('Akyaka','Arpaçay','Digor','Kağızman','Merkez','Sarıkamış','Selim','Susuz'),
+
+		'TR37' => array('Abana','Ağlı','Araç','Azdavay','Bozkurt','Cide','Çatalzeytin','Daday','Devrekani','Doğanyurt','Hanönü','İhsangazi','İnebolu','Küre','Merkez','Pınarbaşı','Seydiler','Şenpazar','Taşköprü','Tosya'),
+
+		'TR38' => array('Akkışla','Bünyan','Develi','Felahiye','Hacılar','İncesu','Kocasinan','Melikgazi','Özvatan','Pınarbaşı','Sarıoğlan','Sarız','Talas','Tomarza','Yahyalı','Yeşilhisar'),
+
+		'TR39' => array('Babaeski','Demirköy','Kofçaz','Lüleburgaz','Merkez','Pehlivanköy','Pınarhisar','Vize'),
+
+		'TR40' => array('Akçakent','Akpınar','Boztepe','Çiçekdağı','Kaman','Merkez','Mucur'),
+
+		'TR41' => array('Başiskele','Çayırova','Darıca','Derince','Dilovası','Gebze','Gölcük','İzmit','Kandıra','Karamürsel','Kartepe','Körfez','Merkez'),
+
+		'TR42' => array('Ahırlı','Akören','Akşehir','Altınekin','Beyşehir','Bozkır','Cihanbeyli','Çeltik','Çumra','Derbent','Derebucak','Doğanhisar','Emirgazi','Ereğli','Güneysınır','Hadım','Halkapınar','Hüyük','Ilgın','Kadınhanı','Karapınar','Karatay','Kulu','Meram','Sarayönü','Selçuklu','Seydişehir','Taşkent','Tuzlukçu','Yalıhüyük','Yunak'),
+
+		'TR43' => array('Altıntaş','Aslanapa','Çavdarhisar','Domaniç','Dumlupınar','Emet','Gediz','Hisarcık','Merkez','Pazarlar','Şaphane','Simav','Tavşanlı'),
+
+		'TR44' => array('Akçadağ','Arapgir','Arguvan','Battalgazi','Darende','Doğanşehir','Doğanyol','Hekimhan','Kale','Kuluncak','Pütürge','Yazıhan','Yeşilyurt'),
+
+		'TR45' => array('Ahmetli','Akhisar','Alaşehir','Demirci','Gölmarmara','Gördes','Kırkağaç','Köprübaşı','Kula','Salihli','Sarıgöl','Saruhanlı','Selendi','Soma','Şehzadeler','Turgutlu','Yunusemre'),
+
+		'TR46' => array('Afşin','Andırın','Çağlayancerit','Dulkadiroğlu','Ekinözü','Elbistan','Göksun','Nurhak','Onikişubat','Pazarcık','Türkoğlu'),
+
+		'TR47' => array('Artuklu','Dargeçit','Derik','Kızıltepe','Mazıdağı','Midyat','Nusaybin','Ömerli','Savur','Yeşilli'),
+
+		'TR48' => array('Bodrum','Dalaman','Datça','Fethiye','Kavaklıdere','Köyceğiz','Marmaris','Menteşe','Milas','Ortaca','Seydikemer','Ula','Yatağan'),
+
+		'TR49' => array('Bulanık','Hasköy','Korkut','Malazgirt','Merkez','Varto'),
+
+		'TR50' => array('Acıgöl','Avanos','Derinkuyu','Gülşehir','Hacıbektaş','Kozaklı','Merkez','Ürgüp'),
+
+		'TR51' => array('Altunhisar','Bor','Çamardı','Çiftlik','Merkez','Ulukışla'),
+
+		'TR52' => array('Akkuş','Altınordu','Aybastı','Çamaş','Çatalpınar','Çaybaşı','Fatsa','Gölköy','Gülyalı','Gürgentepe','İkizce','Kabadüz','Kabataş','Korgan','Kumru','Mesudiye','Perşembe','Ulubey','Ünye'),
+
+		'TR53' => array('Ardeşen','Çamlıhemşin','Çayeli','Derepazarı','Fındıklı','Güneysu','Hemşin','İkizdere','İyidere','Kalkandere','Merkez','Pazar'),
+
+		'TR54' => array('Adapazarı','Akyazı','Arifiye','Erenler','Ferizli','Geyve','Hendek','Karapürçek','Karasu','Kaynarca','Kocaali','Pamukova','Sapanca','Serdivan','Söğütlü','Taraklı'),
+
+		'TR55' => array('Alaçam','Asarcık','Atakum','Ayvacık','Bafra','Canik','Çarşamba','Havza','İlkadım','Kavak','Ladik','Ondokuzmayıs','Salıpazarı','Tekkeköy','Terme','Vezirköprü','Yakakent'),
+
+		'TR56' => array('Merkez','Tillo','Baykan','Eruh','Kurtalan','Pervari','Şirvan'),
+
+		'TR57' => array('Ayancık','Boyabat','Dikmen','Durağan','Erfelek','Gerze','Merkez','Saraydüzü','Türkeli'),
+
+		'TR58' => array('Akıncılar','Altınyayla','Divriği','Doğanşar','Gemerek','Gölova','Hafik','İmranlı','Kangal','Koyulhisar','Merkez','Suşehri','Şarkışla','Ulaş','Yıldızeli','Zara','Gürün'),
+
+		'TR59' => array('Çerkezköy','Çorlu','Ergene','Hayrabolu','Kapaklı','Malkara','Marmara Ereğlisi','Muratlı','Saray','Süleymanpaşa','Şarköy'),
+
+		'TR60' => array('Almus','Artova','Başçiftlik','Erbaa','Merkez','Niksar','Pazar','Reşadiye','Sulusaray','Turhal','Yeşilyurt','Zile'),
+
+		'TR61' => array('Akçaabat','Araklı','Arsin','Beşikdüzü','Çarşıbaşı','Çaykara','Dernekpazarı','Düzköy','Hayrat','Köprübaşı','Maçka','Of','Ortahisar','Sürmene','Şalpazarı','Tonya','Vakfıkebir','Yomra'),
+
+		'TR62' => array('Çemişgezek','Hozat','Mazgirt','Merkez','Nazımiye','Ovacık','Pertek','Pülümür'),
+
+		'TR63' => array('Akçakale','Birecik','Bozova','Ceylanpınar','Eyyübiye','Halfeti','Haliliye','Harran','Hilvan','Karaköprü','Siverek','Suruç','Viranşehir'),
+
+		'TR64' => array('Banaz','Eşme','Karahallı','Merkez','Sivaslı','Ulubey'),
+
+		'TR65' => array('Bahçesaray','Başkale','Çaldıran','Çatak','Edremit','Erciş','Gevaş','Gürpınar','İpekyolu','Merkez','Muradiye','Özalp','Saray','Tuşba'),
+
+		'TR66' => array('Akdağmadeni','Aydıncık','Boğazlıyan','Çandır','Çayıralan','Çekerek','Kadışehri','Merkez','Saraykent','Sarıkaya','Sorgun','Şefaatli','Yenifakılı','Yerköy'),
+
+		'TR67' => array('Alaplı','Çaycuma','Devrek','Gökçebey','Kilimli','Kozlu','Karadeniz Ereğli','Merkez'),
+
+		'TR68' => array('Ağaçören','Merkez','Eskil','Gülağaç','Güzelyurt','Ortaköy','Sarıyahşi'),
+
+		'TR69' => array('Aydıntepe','Merkez','Demirözü'),
+
+		'TR70' => array('Ayrancı','Başyayla','Ermenek','Kazımkarabekir','Merkez','Sarıveliler'),
+
+		'TR71' => array('Bahşılı','Balışeyh','Çelebi','Delice','Karakeçili','Keskin','Merkez','Sulakyurt','Yahşihan'),
+
+		'TR72' => array('Beşiri','Gercüş','Hasankeyf','Kozluk','Merkez','Sason'),
+
+		'TR73' => array('Beytüşşebap','Cizre','Güçlükonak','İdil','Merkez','Silopi','Uludere'),
+
+		'TR74' => array('Amasra','Bartın','Kurucaşile','Ulus','Merkez'),
+
+		'TR75' => array('Merkez','Çıldır','Damal','Göle','Hanak','Posof'),
+
+		'TR76' => array('Aralık','Karakoyunlu','Merkez','Tuzluca'),
+
+		'TR77' => array('Altınova','Armutlu','Çınarcık','Çiftlikköy','Merkez','Termal'),
+
+		'TR78' => array('Eflani','Eskipazar','Merkez','Ovacık','Safranbolu','Yenice'),
+
+		'TR79' => array('Elbeyli','Merkez','Musabeyli','Polateli'),
+
+		'TR80' => array('Bahçe','Düziçi','Hasanbeyli','Kadirli','Merkez','Sumbas','Toprakkale'),
+
+		'TR81' => array('Akçakoca','Cumayeri','Çilimli','Merkez','Gölyaka','Gümüşova','Kaynaşlı','Merkez','Yığılca'),
+
+		);
